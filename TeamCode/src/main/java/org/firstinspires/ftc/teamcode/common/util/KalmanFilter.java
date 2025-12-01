89d7804f5a27cf4407ac895ecd5c0385ba65ce0f
Maths-and-Physics-Are-Fun <CD_au@yahoo.com>
2025-12-01 21:35:02 +1000

    filter

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/util/KalmanFilter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/util/KalmanFilter.java
@@ -3,20 +3,12 @@ package org.firstinspires.ftc.teamcode.common.util;
 import com.arcrobotics.ftclib.command.PerpetualCommand;
 import com.arcrobotics.ftclib.command.RunCommand;
 import com.arcrobotics.ftclib.command.SubsystemBase;
-import com.arcrobotics.ftclib.geometry.Pose2d;
-import com.arcrobotics.ftclib.util.Timing;
-import com.pedropathing.geometry.PedroCoordinates;
-import com.pedropathing.geometry.Pose;
-import com.pedropathing.util.NanoTimer;
 import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
-import com.qualcomm.hardware.sparkfun.SparkFunOTOS;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.hardware.motors.GoBILDA5201Series;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
 import org.firstinspires.ftc.teamcode.common.OpModeReference;
-import org.firstinspires.ftc.teamcode.common.subsystems.PedroPathing;
 
 public class KalmanFilter extends SubsystemBase {
     public static final KalmanFilter INSTANCE = new KalmanFilter();
@@ -31,7 +23,8 @@ public class KalmanFilter extends SubsystemBase {
     double qy = 0;
     double Kx = 0;
     double Ky = 0;
-    double r = 0.0001;
+    double rPinpoint = 0.0001;
+    double rLimelight = 0.001;
 
     long currenttime = 0;
     long previoustime = 0; // better way to do time?
@@ -39,12 +32,13 @@ public class KalmanFilter extends SubsystemBase {
     double ReqAngle = 0;
 
     ElapsedTime Timer;
+    GoBildaPinpointDriver pinpointDriver;
 
     public KalmanFilter() {
         Timer = new ElapsedTime();
         Timer.reset();
+        pinpointDriver = new OpModeReference().getHardwareMap().get(GoBildaPinpointDriver.class,"pinpoint");
         setDefaultCommand(new PerpetualCommand(calculateAngle()));
-
     }
 
     public RunCommand calculateAngle(){
@@ -66,23 +60,48 @@ public class KalmanFilter extends SubsystemBase {
             qy = Vary * ((currenttime-previoustime)*(currenttime-previoustime));
             Vary = Vary + qy;
 
-            //Calculate Kalman Gain
-            Kx=(Varx/(Varx+r));
-            Ky=(Vary/(Vary+r));
 
-            x = prevx + Kx*(OpModeReference.getInstance().pedroPathing.getX()-prevx);
-            y = prevy + Ky*(OpModeReference.getInstance().pedroPathing.getY()-prevy);
+            if (pinpointDriver.getDeviceStatus() == GoBildaPinpointDriver.DeviceStatus.READY) {
+                //Calculate Kalman Gain
+                Kx = (Varx / (Varx + rPinpoint));
+                Ky = (Vary / (Vary + rPinpoint));
 
-            //Update Variance
 
-            Varx *= (1-Kx);
-            Vary *= (1-Ky);
+                x = prevx + Kx * (OpModeReference.getInstance().pedroPathing.getX() - prevx);
+                y = prevy + Ky * (OpModeReference.getInstance().pedroPathing.getY() - prevy);
 
-            // Calculate angle
-            if (OpModeReference.getInstance().isRedAlliance) {
-                ReqAngle = Math.toDegrees(Math.atan((144 - x) / (144 - y)));
-            } else{
-                ReqAngle = Math.toDegrees(Math.atan((-x) / (144 - y)));
+                //Update Variance
+
+                Varx *= (1 - Kx);
+                Vary *= (1 - Ky);
+
+                // Calculate angle
+                if (OpModeReference.getInstance().isRedAlliance) {
+                    ReqAngle = Math.toDegrees(Math.atan((144 - x) / (144 - y)));
+                } else {
+                    ReqAngle = Math.toDegrees(Math.atan((-x) / (144 - y)));
+                }
+            }
+            if (OpModeReference.getInstance().limelightSubsystem.resultIsValid) {
+                //Calculate Kalman Gain
+                Kx = (Varx / (Varx + rLimelight));
+                Ky = (Vary / (Vary + rLimelight));
+
+
+                x = prevx + Kx * (OpModeReference.getInstance().limelightSubsystem. - prevx);
+                y = prevy + Ky * (OpModeReference.getInstance().limelightSubsystem. - prevy);
+
+                //Update Variance
+
+                Varx *= (1 - Kx);
+                Vary *= (1 - Ky);
+
+                // Calculate angle
+                if (OpModeReference.getInstance().isRedAlliance) {
+                    ReqAngle = Math.toDegrees(Math.atan((144 - x) / (144 - y)));
+                } else {
+                    ReqAngle = Math.toDegrees(Math.atan((-x) / (144 - y)));
+                }
             }
         }, this);
     }
